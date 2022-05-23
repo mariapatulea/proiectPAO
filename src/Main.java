47ad084c0bc66c98f4c9ee4bd00a27004848eb80
mariapatulea <mariaelizapatulea@gmail.com>
2022-05-23 19:45:38 +0300

    last update (etapa 3 done)

--- src/Main.java
+++ src/Main.java
@@ -8,67 +8,67 @@ import static java.lang.Integer.parseInt;
 
 public class Main {
     public static void main(String[] args) {
-        // 8 tipuri de obiecte diferite
-        Agentie aPWatt = new Agentie("A P Watt");
-        Agent emilySmith = new Agent("Emily", "Smith", aPWatt);
-        Set<Carte> cartiAlexandraBracken = new HashSet<>();
-        Editura hyperion = new Editura("Hyperion");
-        Editor robLowe = new Editor("Rob", "Lowe", hyperion);
-        Autor alexandraBracken = new Autor("Alexandra", "Bracken", emilySmith, robLowe, cartiAlexandraBracken);
-        Carte lore = new Carte("Lore", alexandraBracken, 1, hyperion, 480, "1484778200",
-                13, true);
-        cartiAlexandraBracken.add(lore);
-
-        Set<Carte> cartiBrigidKemmerer = new HashSet<>();
-        Editura bloomsburyYA = new Editura("Bloomsbury YA");
-        Editor clareMoss = new Editor("Clare", "Moss", bloomsburyYA);
-        Autor brigidKemmerer = new Autor("Brigid", "Kemmerer", emilySmith, clareMoss,
-                cartiBrigidKemmerer);
-        Carte defyTheNight = new Carte("Defy the Night", brigidKemmerer, 1, bloomsburyYA,
-                448, "1547604670", 5, true);
-        cartiBrigidKemmerer.add(defyTheNight);
-
-        Carte defendTheDawn = new Carte("Defend the Dawn", brigidKemmerer, 2, bloomsburyYA,
-                496, "9781547610082", 8, false);
-        cartiBrigidKemmerer.add(defendTheDawn);
-
-//        brigidKemmerer.afisare();
-
-//        lore.afisare();
-
-//        lore.getAutor().afisare();
-
-        List<Carte> cartiImprMariaPatulea = new ArrayList<Carte>();
-        cartiImprMariaPatulea.add(lore);
-        cartiImprMariaPatulea.add(defendTheDawn);
-        lore.setNumarExemplare(lore.getNumarExemplare() - 1);
-        defendTheDawn.setNumarExemplare(defendTheDawn.getNumarExemplare() - 1);
-        Cititor mariaPatulea = new Cititor("Maria", "Pătulea", 1, cartiImprMariaPatulea);
-//        mariaPatulea.afisare();
-
-        List<Carte> cartiFictiune = new ArrayList<Carte>();
-        cartiFictiune.add(lore);
-        cartiFictiune.add(defyTheNight);
-        cartiFictiune.add(defendTheDawn);
-        Sectiune fiction = new Sectiune("Fiction (Fictiune)", cartiFictiune);
-
-        List<Carte> cartiFantasy = new ArrayList<Carte>();
-        cartiFantasy.add(lore);
-        Subsectiune fantasy = new Subsectiune("Fantasy (Fantezie)", cartiFantasy);
-        
-
-//        fiction.afisare();
-//        fantasy.afisare();
-
-        List<Carte> cartiImprAlexIonescu = new ArrayList<Carte>();
-        cartiImprAlexIonescu.add(lore);
-        lore.setNumarExemplare(lore.getNumarExemplare() - 1);
-        Guest alexIonescu = new Guest("Alex", "Ionescu", 2, cartiImprAlexIonescu);
-        alexIonescu.imprumuta(defendTheDawn);  // ok
-        alexIonescu.imprumuta(defyTheNight);  // nu se mai poate imprumta
-        alexIonescu.returneaza(lore);  // ok
-        alexIonescu.imprumuta(defyTheNight);  // ok
-        System.out.println();
+//        // 8 tipuri de obiecte diferite
+//        Agentie aPWatt = new Agentie("A P Watt");
+//        Agent emilySmith = new Agent("Emily", "Smith", aPWatt);
+//        Set<Carte> cartiAlexandraBracken = new HashSet<>();
+//        Editura hyperion = new Editura("Hyperion");
+//        Editor robLowe = new Editor("Rob", "Lowe", hyperion);
+//        Autor alexandraBracken = new Autor("Alexandra", "Bracken", emilySmith, robLowe, cartiAlexandraBracken);
+//        Carte lore = new Carte("Lore", alexandraBracken, 1, hyperion, 480, "1484778200",
+//                13, true);
+//        cartiAlexandraBracken.add(lore);
+//
+//        Set<Carte> cartiBrigidKemmerer = new HashSet<>();
+//        Editura bloomsburyYA = new Editura("Bloomsbury YA");
+//        Editor clareMoss = new Editor("Clare", "Moss", bloomsburyYA);
+//        Autor brigidKemmerer = new Autor("Brigid", "Kemmerer", emilySmith, clareMoss,
+//                cartiBrigidKemmerer);
+//        Carte defyTheNight = new Carte("Defy the Night", brigidKemmerer, 1, bloomsburyYA,
+//                448, "1547604670", 5, true);
+//        cartiBrigidKemmerer.add(defyTheNight);
+//
+//        Carte defendTheDawn = new Carte("Defend the Dawn", brigidKemmerer, 2, bloomsburyYA,
+//                496, "9781547610082", 8, false);
+//        cartiBrigidKemmerer.add(defendTheDawn);
+//
+////        brigidKemmerer.afisare();
+//
+////        lore.afisare();
+//
+////        lore.getAutor().afisare();
+//
+//        List<Carte> cartiImprMariaPatulea = new ArrayList<Carte>();
+//        cartiImprMariaPatulea.add(lore);
+//        cartiImprMariaPatulea.add(defendTheDawn);
+//        lore.setNumarExemplare(lore.getNumarExemplare() - 1);
+//        defendTheDawn.setNumarExemplare(defendTheDawn.getNumarExemplare() - 1);
+//        Cititor mariaPatulea = new Cititor("Maria", "Pătulea", 1, cartiImprMariaPatulea);
+////        mariaPatulea.afisare();
+//
+//        List<Carte> cartiFictiune = new ArrayList<Carte>();
+//        cartiFictiune.add(lore);
+//        cartiFictiune.add(defyTheNight);
+//        cartiFictiune.add(defendTheDawn);
+//        Sectiune fiction = new Sectiune("Fiction (Fictiune)", cartiFictiune);
+//
+//        List<Carte> cartiFantasy = new ArrayList<Carte>();
+//        cartiFantasy.add(lore);
+//        Subsectiune fantasy = new Subsectiune("Fantasy (Fantezie)", cartiFantasy);
+//
+//
+////        fiction.afisare();
+////        fantasy.afisare();
+//
+//        List<Carte> cartiImprAlexIonescu = new ArrayList<Carte>();
+//        cartiImprAlexIonescu.add(lore);
+//        lore.setNumarExemplare(lore.getNumarExemplare() - 1);
+//        Guest alexIonescu = new Guest("Alex", "Ionescu", 2, cartiImprAlexIonescu);
+//        alexIonescu.imprumuta(defendTheDawn);  // ok
+//        alexIonescu.imprumuta(defyTheNight);  // nu se mai poate imprumta
+//        alexIonescu.returneaza(lore);  // ok
+//        alexIonescu.imprumuta(defyTheNight);  // ok
+//        System.out.println();
 
         // afisare comenzi pentru actiuni
         Scanner console = new Scanner(System.in);
@@ -94,11 +94,17 @@ public class Main {
         System.out.println("\nIntroduce un numar intre 1 - 19 pentru a alege una din aceste comenzi, sau 0 pentru a " +
                 "parasi meniul: ");
         String comanda = console.next();
-        System.out.println("\nAi ales comanda " + comenziDisponibile.get((parseInt(comanda)) - 1) + ".");
+        if (!comanda.equals("0")) {
+            System.out.println("\nAi ales comanda " + comenziDisponibile.get((parseInt(comanda)) - 1) + ".");
+        }
 
         // realizare actiuni
         try {
             switch (comanda) {
+                case "0": {
+                    System.out.println("\nAi parasit meniul.");
+                    break;
+                }
                 case "1": {
                     serviciu.adaugare_cititor_membru(console);
                     break;
